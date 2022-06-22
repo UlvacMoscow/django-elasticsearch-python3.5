5baaace969f5c7956bde1a553ea644258ebe7aae
Arseney Abraamyan <ar.abraamyan@gmail.com>
2022-06-22 14:51:30 +0300

    delete str wrapper

--- django_elasticsearch/serializers.py
+++ django_elasticsearch/serializers.py
@@ -145,7 +145,7 @@ class EsModelToJsonMixin(object):
             return obj
 
         # Fallback on a dict with id + __unicode__ value of the related model instance.
-        return dict(id=rel.pk, value=str(rel))
+        return dict(id=rel.pk, value=rel)
 
     def format(self, instance):
         # from a model instance to a dict
